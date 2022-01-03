a8a1cb5c75ff42b37c85a63ddb912fb95158f752
abdur-razzak2672 <abdur35-2672@diu.edu.bd>
2022-01-03 19:14:17 +0600

    added code

--- rest_api/views.py
+++ rest_api/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse , JsonResponse
 from rest_framework.parsers import JSONParser
+from django.views.decorators.csrf import csrf_exempt
 from .models import*
 from .serializers import*
 
