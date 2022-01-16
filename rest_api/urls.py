ef18aafe0a8e571b97a1316f825cb968e218b6d9
abdur-razzak2672 <abdur35-2672@diu.edu.bd>
2022-01-17 01:37:50 +0600

    class based view

--- rest_api/urls.py
+++ rest_api/urls.py
@@ -3,7 +3,8 @@ from .views import *
 
 
 urlpatterns = [
-    path('article/',article_list),
+    #path('article/',article_list),
     path('article/',ArticleAPIView.as_view()),
-    path('detail/<int:pk>/',article_detail),
+    #path('detail/<int:pk>/',article_detail),
+    path('detail/<int:id>/',articleDetails.as_view()),
 ]
--- rest_api/views.py
+++ rest_api/views.py
@@ -26,6 +26,39 @@ class  ArticleAPIView(APIView):
             return Response(serializer.data , status =status.HTTP_201_CREATED)
         return Response(serializer.errors , status =status.HTTP_400_BAD_REQUEST)
 
+class articleDetails(APIView):
+    def get_object(self, id):
+        try:
+            return Article.objects.get(id=id)
+
+        except Article.DoesNotExist:
+            return HttpResponse(status=status.HTTP_404_NOT_FOUND)
+   
+    def get(self, request, id):
+        article = self.get_object(id)
+        serializer = ArticleSerializer(article)
+        return Response(serializer.data)
+    
+    def put(self, request, id) :
+        article = self.get_object(id)
+        serializer = ArticleSerializer(article, data = request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors , status =status.HTTP_400_BAD_REQUEST)
+   
+    def delete(self, request, id) :
+        article = self.get_object(id)
+        article.delete()
+        return Response(status = status.HTTP_204_NO_CONTENT)
+
+
+        
+
+
+
+
+
 
 
 
@@ -99,7 +132,3 @@ def article_detail(request, pk):
             article.delete()
             return Response(status = status.HTTP_204_NO_CONTENT)
             #return JsonResponse(status =  204)
-    elif request.method == 'DELETE':
-        article.delete()
-        return Response(status = status.HTTP_204_NO_CONTENT)
-        #return JsonResponse(status =  204)
