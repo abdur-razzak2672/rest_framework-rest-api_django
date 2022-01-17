e04684ba1f087b53649b34b2ee434581e570db7e
abdur-razzak2672 <abdur35-2672@diu.edu.bd>
2022-01-17 22:39:32 +0600

    generic & mixins based api

--- rest_api/urls.py
+++ rest_api/urls.py
@@ -3,8 +3,10 @@ from .views import *
 
 
 urlpatterns = [
-    #path('article/',article_list),
-    path('article/',ArticleAPIView.as_view()),
-    #path('detail/<int:pk>/',article_detail),
+    #path('article/',article_list), funtion view api
+    #path('article/',ArticleAPIView.as_view()), class view api
+    #path('detail/<int:pk>/',article_detail),funtion view api
     path('detail/<int:id>/',articleDetails.as_view()),
+
+    path('generic/article/<int:id>',GenericAPIView.as_view()),
 ]
--- rest_api/views.py
+++ rest_api/views.py
@@ -8,6 +8,33 @@ from .models import*
 from .serializers import*
 from rest_framework.response import Response
 from rest_framework.views import APIView
+from rest_framework import generics
+from rest_framework import mixins
+
+#generics & mixins based api
+
+class GenericAPIView(generics.GenericAPIView, mixins.ListModelMixin, mixins.CreateModelMixin,mixins.UpdateModelMixin,mixins.DestroyModelMixin,mixins.RetrieveModelMixin):
+    serializer_class = ArticleSerializer
+    queryset = Article.objects.all() 
+    lookup_field = 'id'
+
+    def get(self ,request,id=None):
+        if id:
+            return self.retrieve(request)
+        else:
+            return self.list(request)
+
+    def post(self ,request):
+        return self.create(request)
+
+    def put(self ,request, id=None):
+        return self.update(request,id)
+
+    def delete(self ,request,id):
+        return self.destroy(request,id)
+    
+
+
 
 #class based api view
 
