37fca26a53ae2363433c6fefed9179e77e4c01c6
abdur-razzak2672 <abdur35-2672@diu.edu.bd>
2022-01-18 21:29:00 +0600

    Authentications

--- rest_api/views.py
+++ rest_api/views.py
@@ -10,6 +10,8 @@ from rest_framework.response import Response
 from rest_framework.views import APIView
 from rest_framework import generics
 from rest_framework import mixins
+from rest_framework.authentication import SessionAuthentication , BasicAuthentication, TokenAuthentication
+from rest_framework.permissions import IsAuthenticated
 
 #generics & mixins based api
 
@@ -17,6 +19,9 @@ class GenericAPIView(generics.GenericAPIView, mixins.ListModelMixin, mixins.Crea
     serializer_class = ArticleSerializer
     queryset = Article.objects.all() 
     lookup_field = 'id'
+    #authentication_classes = [SessionAuthentication, BasicAuthentication]
+    authentication_classes = [TokenAuthentication]
+    permission_classes = [IsAuthenticated]
 
     def get(self ,request,id=None):
         if id:
